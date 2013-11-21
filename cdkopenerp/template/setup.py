bd0ae2e3574f69f726a168ba7ab2177bb72696e1
Jemai Hamza <jha@MBP-JHA>
2013-11-21 14:12:08 +0100

    jha

--- cdkopenerp/template/setup.py
+++ cdkopenerp/template/setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(name='openerpapp', version='1.0',
-      description='OpenERP Python-2.7 Community Cartridge based application',
+      description='OpenShift Python-2.7 Community Cartridge based application',
       author='jha', author_email='jha@pcsol.tn',
       url='http://www.python.org/sigs/distutils-sig/',
 
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
-      install_requires=['greenlet', 'postgresql-9.2',
-                        #  'MySQL-python',
-                        #  'pymongo',
-                        #  'psycopg2',
+      install_requires=['greenlet',
+                        'MySQL-python',
+                       'pymongo',
+                        'psycopg2',
       ],
      )
